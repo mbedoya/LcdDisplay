f50ebf49ec50566ad16b3e1841d0682b7dde8c80
mbedoya <mauricio.bedoya@gmail.com>
2017-04-20 12:41:20 -0500

    HtmlDisplay - Digit "1"

---
+++ LcdDisplay.Model/Displays/HtmlDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LcdDisplay.Model.Displays
+{
+    public class HtmlDisplay
+    {
+        public HtmlDisplay()
+        {
+
+        }
+
+        public String GetFromDigit(int size, string number)
+        {
+            switch (number)
+            {
+                case "1":
+                    return GetDigitOne(size, number);
+                default:
+                    break;
+            }
+            return number;
+        }
+
+        private string GetDigitOne(int size, string number)
+        {
+            return GetBlankSpaces(1) + GetPipes(size) + GetBlankSpaces(1) + GetPipes(size) + GetBlankSpaces(1);
+        }
+
+        private string GetBlankSpaces(int count)
+        {
+            return RepeatString("&nbsp;", count) + "<br />";
+        }
+
+        private string GetPipes(int count)
+        {
+            return RepeatString("|<br />", count);
+        }
+
+        private String RepeatString(string text, int count)
+        {
+            return String.Concat(Enumerable.Repeat(text, count));
+        }
+    }
+}
---
+++ LcdDisplay.Tests/HtmlDisplayTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LcdDisplay.Model.Displays;
+
+namespace LcdDisplay.Tests
+{
+    [TestClass]
+    public class HtmlDisplayTest
+    {
+
+        [TestMethod]
+        public void GetFromDigit_NumberReturnedHasData_ReturnedNumberHasData()
+        {
+            HtmlDisplay display = new HtmlDisplay();
+            String result = display.GetFromDigit(1, "1");
+            Assert.AreEqual(true, !String.IsNullOrEmpty(result));
+        }
+
+        [TestMethod]
+        public void GetFromDigit_NumberInSize2IsWhatExpected_ExpectedText()
+        {
+            HtmlDisplay display = new HtmlDisplay();
+            String result = display.GetFromDigit(2, "1");
+            Assert.AreEqual("&nbsp;<br />|<br />|<br />&nbsp;<br />|<br />|<br />&nbsp;<br />", result);
+        }
+
+        [TestMethod]
+        public void GetFromDigit_NumberInSize1IsWhatExpected_ExpectedText()
+        {
+            HtmlDisplay display = new HtmlDisplay();
+            String result = display.GetFromDigit(1, "1");
+            Assert.AreEqual("&nbsp;<br />|<br />&nbsp;<br />|<br />&nbsp;<br />", result);
+        }
+    }
+}
