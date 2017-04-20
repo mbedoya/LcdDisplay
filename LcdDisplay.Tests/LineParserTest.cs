8cab76ebfbbbb96f555e6832f8e97b72848b4192
mbedoya <mauricio.bedoya@gmail.com>
2017-04-20 10:14:41 -0500

    Firt Basic Test - Does not compile
    Do not do this ever, just for the test history

---
+++ LcdDisplay.Tests/LineParserTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LcdDisplay.Tests
+{
+    [TestClass]
+    public class LineParserTest
+    {
+        [TestMethod]
+        public void Parse_BasicLineIsValid_LineValidResult()
+        {
+            String line = "0,0";
+            LineParserResult result = new LineParser(line).Parse();
+            Assert.AreEqual(true, result.Status == LineParserResult.ValidLine);
+        }
+    }
+}
